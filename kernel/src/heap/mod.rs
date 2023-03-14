79743b99b6d38c2888283da8fffc8284217068e1
Luuk van Oijen <lazyluuk.channel@gmail.com>
2023-03-14 23:02:27 +0100

    Update mod.rs

--- kernel/src/heap/mod.rs
+++ kernel/src/heap/mod.rs
@@ -1,17 +1,8 @@
-use alloc::alloc::{Layout, GlobalAlloc};
-use x86_64::VirtAddr;
-use x86_64::structures::paging::Page;
-use good_memory_allocator::SpinLockedAllocator;
-use kernel_common::Mutex;
-
-use crate::memory;
-
 pub mod linked_list_alloc;
 use linked_list_alloc::*;
 
 #[global_allocator]
 static ALLOCATOR: LinkedListAlloc = LinkedListAlloc::empty();
-// static ALLOCATOR: SpinLockedAllocator = SpinLockedAllocator::empty();
 
 pub const HEAP_START:   usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE:    usize = 1024 * 4096;
